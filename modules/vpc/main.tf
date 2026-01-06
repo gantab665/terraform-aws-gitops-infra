c9262af4da26415985785cea60fcd98dbdaa4611
gantab665 <ganta.b@northeastern.edu>
2026-01-06 18:57:24 -0500

    Step 2.5: Add Internet Gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -23,3 +23,13 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-igw"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
