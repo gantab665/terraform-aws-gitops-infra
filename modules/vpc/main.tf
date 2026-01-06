13118ebf82b564331d511359a22c499ec9998160
gantab665 <ganta.b@northeastern.edu>
2026-01-06 18:56:18 -0500

    Step 2.4: Add public subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -9,3 +9,17 @@ resource "aws_vpc" "this" {
     Project     = var.project_name
   }
 }
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.environment == "dev" ? "us-east-1a" : "us-east-1b"}"
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-public-subnet"
+    Environment = var.environment
+    Project     = var.project_name
+  }
+}
+
