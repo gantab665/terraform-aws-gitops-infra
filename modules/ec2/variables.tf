60e2bcc1c59f170759ccd5e4e721b3c1951322d7
gantab665 <ganta.b@northeastern.edu>
2026-01-06 19:02:38 -0500

    Step 3.2: Define EC2 module variables

---
+++ modules/ec2/variables.tf
@@ -0,0 +1,24 @@
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where EC2 will be deployed"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project name for tagging"
+  type        = string
+}
+
+variable "environment" {
+  description = "Deployment environment (dev, prod)"
+  type        = string
+}
