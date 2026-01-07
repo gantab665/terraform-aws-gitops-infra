a6f21bd07246de53ba484b463eef20003e4c3205
gantab665 <ganta.b@northeastern.edu>
2026-01-06 19:27:46 -0500

    Fix: Use correct Amazon Linux 2 AMI for us-west-2

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -7,3 +7,16 @@ module "vpc" {
   project_name = var.project_name
   environment  = var.environment
 }
+
+module "ec2" {
+  source = "../../modules/ec2"
+
+  ami_id = "ami-0b8c6b923777519db" # Amazon Linux 2 (us-east-1)
+  instance_type = "t2.micro"
+
+  subnet_id = module.vpc.public_subnet_id
+  vpc_id    = module.vpc.vpc_id
+
+  project_name = var.project_name
+  environment  = var.environment
+}
