b951e76f0778ca467561ecc8cba30dcb677af208
gantab665 <ganta.b@northeastern.edu>
2026-01-06 19:25:25 -0500

    Fix: Remove hardcoded availability zone from subnet

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -14,13 +14,13 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.this.id
   cidr_block              = var.public_subnet_cidr
   map_public_ip_on_launch = true
-  availability_zone       = "${var.environment == "dev" ? "us-east-1a" : "us-east-1b"}"
 
   tags = {
     Name        = "${var.project_name}-${var.environment}-public-subnet"
     Environment = var.environment
     Project     = var.project_name
   }
+
 }
 
 resource "aws_internet_gateway" "this" {
